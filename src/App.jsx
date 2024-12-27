b374a43865616773445692e3e0dc9ee47d4d0409
Vaibhav Kotnala <vaibhave31@gmail.com>
2024-12-27 22:23:28 +0530

    nike brand page based on react first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+
+
+function App() {
+  
+
+  return (
+    <>
+     <div>
+      <Navigation />
+      <Hero />
+    </div>
+    </>
+  )
+}
+
+export default App
